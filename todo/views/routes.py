4c2936408218a6657e66a201003d071dac7b7aa6
lucashicks1 <lucasjhicks1@gmail.com>
2025-02-25 08:49:15 +1000

    fix: Returns 201 status code for post request and handles query params in GET request

--- todo/views/routes.py
+++ todo/views/routes.py
@@ -1,5 +1,4 @@
-from flask import Blueprint, jsonify
-
+from flask import Blueprint, jsonify, request
 
 api = Blueprint("api", __name__, url_prefix="/api/v1")
 
@@ -11,13 +10,17 @@ def health():
 
 @api.route("/todos", methods=["GET"])
 def get_todos():
+    args = request.args
+    completed: bool = args.get("completed", default=True, type=bool)
+    window: int = args.get("window", default=0, type=int)  # noqa: F841
+
     return jsonify(
         [
             {
                 "id": 1,
                 "title": "Watch CSSE6400 Lecture",
                 "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
-                "completed": True,
+                "completed": completed,
                 "deadline_at": "2023-02-27T00:00:00",
                 "created_at": "2023-02-20T00:00:00",
                 "updated_at": "2023-02-20T00:00:00",
@@ -28,16 +31,19 @@ def get_todos():
 
 @api.route("/todos", methods=["POST"])
 def create_todo():
-    return jsonify(
-        {
-            "id": 1,
-            "title": "Watch CSSE6400 Lecture",
-            "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
-            "completed": True,
-            "deadline_at": "2023-02-27T00:00:00",
-            "created_at": "2023-02-20T00:00:00",
-            "updated_at": "2023-02-20T00:00:00",
-        }
+    return (
+        jsonify(
+            {
+                "id": 1,
+                "title": "Watch CSSE6400 Lecture",
+                "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
+                "completed": True,
+                "deadline_at": "2023-02-27T00:00:00",
+                "created_at": "2023-02-20T00:00:00",
+                "updated_at": "2023-02-20T00:00:00",
+            }
+        ),
+        201,
     )
 
 
